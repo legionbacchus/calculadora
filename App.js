a9e6f5dd614b3f825e058500ffb19890c11e6c18
Rodrigo Baruch <legionbacchus@gmail.com>
2022-06-27 18:23:15 -0500

    array

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  const buttons = ['AC','()','%','÷',7,8,9,'X',4,5,6,'-',1,2,3,'+',0,'.','DEL','=']
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
